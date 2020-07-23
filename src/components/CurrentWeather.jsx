262274ab34bdc0f0cf01ba0df9cf6727c4e450a0
Joshua Kuestersteffen <jkuester@kuester7.com>
2020-07-23 16:39:07 -0500

    Add current humidity

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactAnimatedWeather from 'react-animated-weather';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLongArrowAltUp } from '@fortawesome/free-solid-svg-icons'
+import { faLongArrowAltDown } from '@fortawesome/free-solid-svg-icons'
 
 const OPEN_WEATHER_ROOT = 'https://api.openweathermap.org/data/2.5/onecall';
 const LAT = '38.910843';
@@ -44,8 +44,10 @@ class WeatherData {
         conditionIcon: 'CLOUDY',
         description: '',
         descriptionShort: '',
+        humidity: 99,
         temp: 999,
         tempFeelsLike: 999,
+        windDeg: 0,
         windSpeed: 999
       },
       daily: [{
@@ -63,9 +65,11 @@ class WeatherData {
           conditionIcon: getWeatherIconOWM(data.current.weather[0].id),
           description: capitalize(data.current.weather[0].description),
           descriptionShort: data.current.weather[0].main,
+          humidity: data.current.humidity,
           tempFeelsLike: Math.round(data.current.feels_like),
           temp: Math.round(data.current.temp),
-          windSpeed: data.current.wind_speed
+          windDeg: data.current.wind_deg,
+          windSpeed: Math.round(data.current.wind_speed)
         };
         this.daily = [{
           maxTemp: Math.round(data.daily[0].temp.max),
@@ -98,7 +102,7 @@ class CurrentWeather extends React.Component {
   render() {
     return (
       <div>
-        <div>Wind: {this.state.weather.current.windSpeed}mph <FontAwesomeIcon icon={faLongArrowAltUp} transform={{ rotate: 42 }}/></div>
+        <div>Wind: {this.state.weather.current.windSpeed}mph <FontAwesomeIcon icon={faLongArrowAltDown} transform={{ rotate: this.state.weather.current.windDeg }}/> Humidity: {this.state.weather.current.humidity}%</div>
         <h2><ReactAnimatedWeather icon={this.state.weather.current.conditionIcon} color='white' size={this.state.iconSizePx}/> {this.state.weather.current.temp}˚ {this.state.weather.current.descriptionShort}</h2>
         <div>Feels Like: {this.state.weather.current.tempFeelsLike}˚ Low: {this.state.weather.daily[0].minTemp}˚ High: {this.state.weather.daily[0].maxTemp}˚</div>
         <div>{this.state.weather.current.description}</div>
