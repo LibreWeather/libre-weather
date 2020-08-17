1c910a2b9318501e649e047a5ccb328cefac41bd
Tobiah <tobiah@warframe.gg>
2020-08-16 21:18:54 -0500

    Add units toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+/* globals fetch */
 
+import React from 'react';
 import CurrentWeather from './components/CurrentWeather';
 import DailyOverview from './components/DailyOverview';
-import LocationSearch from './components/LocationSearch';
 import DEFAULT_WEATHER_DATA from './resources/defaultWeatherData.json';
+import LocationSearch from './components/LocationSearch';
 
 const LIBRE_WEATHER_API_ROOT = process.env.LIBRE_WEATHER_API;
 
@@ -14,37 +15,43 @@ class App extends React.Component {
     this.state = {
       weather: DEFAULT_WEATHER_DATA,
       units: 'IMPERIAL',
+      lat: 37.8180061,
+      lon: -96.8480188,
     };
+
+    this.updateUnits = this.updateUnits.bind(this);
+    this.updateWeather = this.updateWeather.bind(this);
   }
 
-  updateWeather = (lat, lon) => {
-    var headers = { 'x-latitude': lat, 'x-longitude': lon, 'x-unit': this.state.units };
-    fetch(LIBRE_WEATHER_API_ROOT, { method: 'GET', headers: headers })
+  updateUnits(units) {
+    this.setState({ units });
+    const { lat, lon } = this.state;
+    this.updateWeather(lat, lon);
+  }
+
+  updateWeather(lat, lon) {
+    this.setState({ lat, lon });
+
+    const { units } = this.state;
+    const headers = { 'x-latitude': lat, 'x-longitude': lon, 'x-unit': units };
+
+    fetch(LIBRE_WEATHER_API_ROOT, { method: 'GET', headers })
       .then((res) => res.json())
       .then((data) => {
-        // data.hourly[0].condition = 'RAIN';
-        // data.hourly[1].condition = 'RAIN';
-        // data.hourly[2].condition = 'RAIN';
-
-        // data.hourly[7].condition = 'CLEAR';
-        // data.hourly[8].condition = 'CLEAR';
-        // data.hourly[9].condition = 'CLEAR';
-        // data.hourly[10].condition = 'CLEAR';
-
-        this.setState({
-          weather: data,
-        });
+        this.setState({ weather: data });
       })
+      // eslint-disable-next-line no-console
       .catch(console.error);
   };
 
   render() {
+    const { weather } = this.state;
     return (
       <div className="App">
+        <LocationSearch updateWeather={this.updateWeather} updateUnits={this.updateUnits} />
         <header className="header">
-          <LocationSearch updateWeather={this.updateWeather} />
-          <CurrentWeather weatherData={this.state.weather} />
-          <DailyOverview weatherData={this.state.weather} />
+          <CurrentWeather weatherData={this.state.weather}/>
+          <DailyOverview weatherData={this.state.weather}/>
         </header>
       </div>
     );
--- src/components/LocationSearch.jsx
+++ src/components/LocationSearch.jsx
@@ -2,56 +2,100 @@ import React from 'react';
 import * as Nominatim from 'nominatim-browser';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
-import Form from 'react-bootstrap/Form';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as Nominatim from 'nominatim-browser';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 class LocationSearch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { zip: '67042' };
+    this.state = {
+      zip: '67042',
+      locationName: 'El Dorado',
+    };
     this.updateLocation();
+
+    this.updateLocation = this.updateLocation.bind(this);
+    this.handleZipChange = this.handleZipChange.bind(this);
+    this.handleZipSubmit = this.handleZipSubmit.bind(this);
+    this.handleUnitsChange = this.handleUnitsChange.bind(this);
   }
 
-  updateLocation = () => {
+  updateLocation() {
     // TODO This supports a more generic search string
     // Also, should handle multiple results and no results
+    const { zip } = this.state;
+    const { updateWeather } = this.props;
+
     Nominatim.geocode({
-      postalcode: this.state.zip,
-    }).then((results) => this.props.updateWeather(results[0].lat, results[0].lon));
-  };
+      addressdetails: true,
+      postalcode: zip,
+    })
+      .then((results) => {
+        this.state.locationName = results[0].address.city;
+        updateWeather(results[0].lat, results[0].lon);
+      });
+  }
 
-  handleChange = (event) => {
+  handleZipChange(event) {
     this.setState({ zip: event.target.value });
-  };
+  }
 
-  handleSubmit = (event) => {
+  handleZipSubmit(event) {
     this.updateLocation();
     event.preventDefault();
-  };
+    event.target.reset();
+  }
+
+  handleUnitsChange(event) {
+    const units = event.target.checked ? 'METRIC' : 'IMPERIAL';
+    const { updateUnits } = this.props;
+    updateUnits(units);
+  }
 
   render() {
+    const { locationName } = this.state;
+
     return (
-      <Form onSubmit={this.handleSubmit}>
-        <InputGroup className="mb-4">
-          <FormControl
-            placeholder="ZIP Code"
-            aria-label="ZIP Code"
-            aria-describedby="basic-addon2"
-            type="number"
-            onChange={this.handleChange}
-            value={this.state.zip}
-          />
-          <InputGroup.Append>
-            <Button variant="outline-secondary" type="submit">
-              <FontAwesomeIcon icon={faSearch} />
-            </Button>
-          </InputGroup.Append>
-        </InputGroup>
-      </Form>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home" className="py-0">
+          Libre Weather
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto" />
+          <Nav.Link id="locationName" className="py-0" disabled>{locationName}</Nav.Link>
+          <Form onSubmit={this.handleZipSubmit}>
+            <InputGroup>
+              <FormControl placeholder="ZIP Code" aria-label="ZIP Code" aria-describedby="basic-addon2" type="number" onChange={this.handleZipChange} />
+              <InputGroup.Append>
+                <Button variant="outline-secondary" type="submit"><FontAwesomeIcon icon={faSearch} /></Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Form>
+          <Nav.Link className="py-0" disabled>˚F</Nav.Link>
+          <Form>
+            <Form.Switch
+              id="custom-switch"
+              label="˚C"
+              onChange={this.handleUnitsChange}
+            />
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
+LocationSearch.propTypes = {
+  updateWeather: PropTypes.func.isRequired,
+  updateUnits: PropTypes.func.isRequired,
+};
 
 module.exports = LocationSearch;
