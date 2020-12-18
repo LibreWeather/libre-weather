fc4e4e13d404f7d42add812cd81ba8cb6258eb10
Matt Voboril <tobiah@protonmail.com>
2020-12-18 13:46:28 -0600

    feat(views): License view & link to github (#10)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 /* globals fetch, localStorage */
 
 import React from 'react';
+import NavLink, { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faOsi } from '@fortawesome/free-brands-svg-icons';
+
+import DEFAULT_WEATHER_DATA from './resources/defaultWeatherData.json';
+import CurrentDayContext from './utilities/CurrentDayContext';
+
+import NavigationBar from './components/NavigationBar';
 import CurrentWeather from './components/CurrentWeather';
 import DailyOverview from './components/DailyOverview';
 import WeeklyForecast from './components/WeeklyForecast';
-import DEFAULT_WEATHER_DATA from './resources/defaultWeatherData.json';
-import NavigationBar from './components/NavigationBar';
-import CurrentDayContext from './utilities/CurrentDayContext';
+
+import LicensesView from './views/LicensesView';
 
 const LIBRE_WEATHER_API_ROOT = process.env.LIBRE_WEATHER_API;
 const DEFAULT_UNITS = 'IMPERIAL';
@@ -62,16 +73,34 @@ class App extends React.Component {
       sunset: weather.current.sunset,
       time: weather.current.time,
     };
+
     return (
       <CurrentDayContext.Provider value={currentDayData}>
-        <div className="App">
+        <div className="App hide-scroll">
           <NavigationBar setLatLon={this.setLatLon} setUnits={this.setUnits} />
-          <header className="header">
-            <CurrentWeather weatherData={weather} />
-            <DailyOverview hourlyWeatherData={weather.hourly} />
-            <WeeklyForecast weatherData={weather} />
-          </header>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/">
+                <header className="header constrained hide-scroll">
+                  <CurrentWeather weatherData={weather} />
+                  <DailyOverview hourlyWeatherData={weather.hourly} />
+                  <WeeklyForecast weatherData={weather} />
+                </header>
+              </Route>
+              <Route path="/licenses">
+                <LicensesView />
+              </Route>
+            </Switch>
+          </BrowserRouter>
         </div>
+        <Navbar fixed="bottom">
+          <Nav.Link href="/licenses">
+            <FontAwesomeIcon icon={faOsi} />
+          </Nav.Link>
+          <Nav.Link href="https://github.com/LibreWeather/libre-weather" target="_blank">
+            <FontAwesomeIcon icon={faGithub} />
+          </Nav.Link>
+        </Navbar>
       </CurrentDayContext.Provider>
     );
   }
---
+++ src/views/LicensesView.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { Container, Row, Col } from 'react-bootstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMugHot, faCode } from '@fortawesome/free-solid-svg-icons';
+import packages from '../assets/packages.json';
+
+class Pack extends React.Component {
+  render() {
+    const { pack } = this.props;
+    const bit = `${pack.name} : ${pack.license}`;
+    return (
+      <Row key="{pack.name}" className="row-center">
+        <h6>{bit}</h6>
+      </Row>
+    );
+  }
+}
+
+export default class LicensesView extends React.Component {
+  render() {
+    const packs = packages.map((pack) => <Pack pack={pack} />);
+    return (
+      <header className="header">
+        <div>
+          <h2>
+            Built with &nbsp;
+            <FontAwesomeIcon icon={faMugHot} />
+            &nbsp;&&nbsp;
+            <FontAwesomeIcon icon={faCode} />
+          </h2>
+          <br />
+          <Container fluid="sm" className="constrained hide-scroll">
+            {packs}
+          </Container>
+        </div>
+      </header>
+    );
+  }
+}
